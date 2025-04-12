0473dc190a4d3becf9ecae5061a52da337b52251
Massi <massi.djerrah59@gmail.com>
2025-04-12 14:17:21 +0200

    feat(stock): add full stock module with CSV parsing & tests ✨

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from 'src/product/product.module';
+import { StockModule } from 'src/stock/stock.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ProductModule],
+  imports: [
+    ProductModule, 
+    StockModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/stock/stock.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from "@nestjs/common";
+import { StockCsvDto } from "./dto/stock-csv.dto";
+import { parse } from 'csv-parse';
+import axios from 'axios';
+import { CsvRowDto } from "./dto/csv-row.dto";
+import { ConfigService } from "@nestjs/config";
+
+@Injectable()
+export class StockService {
+  constructor(private readonly configService: ConfigService) {}
+  
+  /**
+  * Parse un fichier CSV de stock à partir d'une URL distante.
+  *
+  * @returns Une promesse contenant une liste d'objets de type `StockCsvDto`.
+  * @throws Une erreur si le parsing échoue ou si la récupération du fichier échoue.
+  */
+  async parseStockFromCsv(): Promise<StockCsvDto[]> {
+    try {
+      const url = this.configService.get<string>('CSV_URL');
+      if (!url) {
+        throw new Error('CSV_URL is not defined in the environment configuration');
+      }
+      const response = await axios.get(url, { responseType: 'text' });
+
+      const results: StockCsvDto[] = [];
+      await new Promise<void>((resolve, reject) => {
+        parse(response.data, { columns: true, trim: true, delimiter: ';' })
+          .on('data', (row: CsvRowDto) => {
+            results.push({
+              idProduct: parseInt(row.IdProduct, 10),
+              stock: parseInt(row.stock, 10),   
+            });
+          })
+          .on('end', resolve)
+          .on('error', reject);
+      });
+      return results;
+    } 
+    catch (err) {
+      throw new Error("Failed to parse CSV");
+    }
+  }
+}
