23f09501a9e0ad593b64ff896207a984df95b12b
Massi <massi.djerrah59@gmail.com>
2025-04-12 02:00:41 +0200

    feat(stock): init module ✨

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from 'src/product/product.module';
+import { StockModule } from 'src/stock/stock.module';
 
 @Module({
-  imports: [ProductModule],
+  imports: [ProductModule, StockModule],
   controllers: [AppController],
   providers: [AppService],
 })
